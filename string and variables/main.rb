8c8d01791370bcf55c98569e6a72e478a371b77a
Mohit <mister.mohitaggarwal@gmail.com>
2024-08-12 11:48:28 +0530

    initial commit

---
+++ string and variables/main.rb
@@ -0,0 +1,14 @@
+# print "Hello world\n"
+# puts "My first ruby program"
+
+# first_name = "mohit"
+# last_name = "aggarwal"
+# full_name = first_name + " " + last_name
+
+# puts full_name
+
+# x =1 
+# puts "#{x}"
+age = 20
+
+puts "your age " + age.to_s
